8db117d8c223550301483e4c911bbb7d18396afc
Agustin Sochi <sochi10agu@gmail.com>
2022-02-19 14:26:00 -0300

    solved search elements with results and with not results

---
+++ cypress/integration/1-search.js
@@ -0,0 +1,23 @@
+describe("Search elements", () => {
+    beforeEach(()=>{
+        cy.visit("/");
+    })
+    it("search for elements with multiples results", ()=>{
+        cy.fixture("index").then((index)=> {
+            cy.get(index.searchBox).type("dress");
+            cy.get(index.searchButton).click();
+        })
+        cy.fixture("searchResults").then((searchResult) => {
+            cy.get(searchResult.title).should("contain", "dress");
+        })
+    })
+    it("search for elements with no results", ()=> {
+        cy.fixture("index").then((index)=>{
+            cy.get(index.searchBox).type("qwerty");
+            cy.get(index.searchButton).click();
+        })
+        cy.fixture("searchResults").then((searchResult) => {
+            cy.get(searchResult.alert).should("contain", "No results were found for your search");
+        })
+    })
+})
